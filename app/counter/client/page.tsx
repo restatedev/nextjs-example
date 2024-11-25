057242891bce7b6e934a65beb51cc26c913f7fbe
Nik Nasr <nik@restate.dev>
2024-11-25 22:43:16 +0000

    remove useOptimistic
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- app/counter/client/page.tsx
+++ app/counter/client/page.tsx
@@ -1,30 +1,24 @@
 "use client";
-
 import { counterClient } from "@/restate/client";
 import useSWR from "swr";
 import Form from "next/form";
-import { useOptimistic } from "react";
 import { Input } from "@/components/Input";
 import { SubmitButton } from "@/components/SubmitButton";
 import { Badge } from "@/components/Badge";
 
 export default function () {
-  const { data: currentValue, mutate } = useSWR(
+  const {
+    data: currentValue,
+    mutate,
+    isLoading,
+  } = useSWR(
     "/Counter/count/current",
     async () => await counterClient.current()
-  );
-
-  const [optimisticValue, addOptimistically] = useOptimistic<
-    number | undefined,
-    number
-  >(currentValue, (state, amount) =>
-    state !== undefined ? state + amount : undefined
   );
 
   const formAction = async (formData: FormData) => {
     const amount = Number(formData.get("amount"));
     if (!isNaN(amount)) {
-      addOptimistically(amount);
       const newValue = await counterClient.add(amount);
       mutate(newValue);
     }
@@ -33,7 +27,7 @@ export default function () {
   return (
     <Form action={formAction} className="flex flex-col gap-4">
       <output className="font-medium text-sm">
-        Current value: <Badge>{optimisticValue ?? "…"}</Badge>
+        Current value: <Badge>{isLoading ? "…" : currentValue}</Badge>
       </output>
       <Input
         type="number"
