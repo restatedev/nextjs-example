25a1d6adfe76ecc8265677bc12910ff1feb66950
Nik Nasr <nik@restate.dev>
2025-04-08 11:45:07 +0100

    update deps
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- components/Input.tsx
+++ components/Input.tsx
@@ -14,7 +14,7 @@ export function Input({
         <input
           {...props}
           id={id}
-          className="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+          className="block w-full rounded-xl border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
         />
       </div>
     </div>
